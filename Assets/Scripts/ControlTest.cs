430f42167e0fa0dff40b6dde69457ac7cdfde1bb
Halaa <neil841004@gmail.com>
2019-11-09 13:21:07 +0800

    Input

---
+++ Assets/Scripts/ControlTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class ControlTest : MonoBehaviour
+{
+    int playerID = 0;
+    Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = ReInput.players.GetPlayer(playerID);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float AimX = player.GetAxis("AimHorizontal");
+        float AimY = player.GetAxis("AimVertical");
+    }
+}
