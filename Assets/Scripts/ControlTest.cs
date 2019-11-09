657af87f227a2b816661e86f3950f23eb78f7214
ChinJianGin <48975062+ChinJianGin@users.noreply.github.com>
2019-11-09 20:34:52 +0800

    textPlayer

--- Assets/Scripts/ControlTest.cs
+++ Assets/Scripts/ControlTest.cs
@@ -5,12 +5,22 @@ using Rewired;
 
 public class ControlTest : MonoBehaviour
 {
+    public float rotationSpeed;
+    public GameObject aimIcon;
+    Rigidbody2D aimIcon_rigidBody2D;
     int playerID = 0;
+    float i_x;
+    float angle;
+    bool canRotation;
     Player player;
     // Start is called before the first frame update
     void Start()
     {
         player = ReInput.players.GetPlayer(playerID);
+        aimIcon_rigidBody2D = GetComponent<Rigidbody2D>();
+        angle = 0.0f;
+        i_x = -0.05f;
+        canRotation = true;
     }
 
     // Update is called once per frame
@@ -18,5 +28,53 @@ public class ControlTest : MonoBehaviour
     {
         float AimX = player.GetAxis("AimHorizontal");
         float AimY = player.GetAxis("AimVertical");
+
+        // Debug.Log("X=");
+        // Debug.Log(AimX);
+        // Debug.Log("Y=");
+        // Debug.Log(AimY);
+
+        if (AimX == 0 && AimY == 0)
+        {
+            aimIcon.GetComponent<BoxCollider2D>().enabled = false;
+            aimIcon.GetComponent<SpriteRenderer>().enabled = false;
+            aimIcon.SendMessage("reduceScale", i_x);
+            Debug.Log(AimX);
+        }
+        else if (((AimX == 0 && AimY > 0) || (AimX > 0 && AimY > 0) || (AimY == 0 && AimX > 0)) && canRotation)
+        {
+            aimIcon.GetComponent<BoxCollider2D>().enabled = true;
+            aimIcon.GetComponent<SpriteRenderer>().enabled = true;
+            //aimIcon.transform.position = new Vector3(AimX, AimY, 0.0f);
+            angle = Mathf.Atan2(AimY, AimX) * Mathf.Rad2Deg;
+            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
+
+
+
+            Debug.Log("X=");
+            Debug.Log(AimX);
+            Debug.Log("Y=");
+            Debug.Log(AimY);
+            Debug.Log("angle=");
+            Debug.Log(angle);
+
+        }
+        else
+        {
+            aimIcon.GetComponent<BoxCollider2D>().enabled = false;
+            aimIcon.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        if (player.GetButton("Shoot"))
+        {
+            aimIcon.SendMessage("aimRayManager", angle);
+            canRotation = false;
+        }
+        else
+        {
+            aimIcon.SendMessage("reduceScale", i_x);
+            canRotation = true;
+        }
+
+
     }
 }
---
+++ Assets/_aimRayManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _aimRayManager : MonoBehaviour
+{
+    public GameObject Raybar;
+    float i; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+    void aimRayManager(float angle){
+        i += 0.1f;
+        this.transform.rotation = Quaternion.Euler(0.0f , 0.0f , angle);
+        Raybar.transform.rotation = Quaternion.Euler(0.0f , 0.0f , angle);
+        Raybar.transform.localScale = new Vector2(i , 0.05f);
+    }
+    void reduceScale(float i_x){
+        i = i + i_x;
+        if(i < 0) i = 0;
+        Raybar.transform.localScale = new Vector2(i , 0.05f);
+    }
+}
